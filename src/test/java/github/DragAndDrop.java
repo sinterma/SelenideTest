bebc361dd8641fe7bb9b3f042247a1c074f0fd78
kvfedot4 <kvfedot4@stream.ru>
2021-05-31 17:34:53 +0300

    Hey there!

--- src/test/java/github/DragAndDrop.java
+++ src/test/java/github/DragAndDrop.java
@@ -4,8 +4,8 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.actions;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
 
@@ -13,9 +13,9 @@ public class DragAndDrop {
     void DragAndDroptest() {
 
         //открыть страницу сайта
-        Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
-        Selenide.$("[id=column-a]").dragAndDropTo("[id=column-b]");
-        Selenide.$("[id=column-a]").shouldHave(Condition.text("B"));
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("[id=column-a]").dragAndDropTo("[id=column-b]");
+        $("[id=column-a]").shouldHave(text("B"));
 
 
     }
--- src/test/java/github/FindSelenideTest.java
+++ src/test/java/github/FindSelenideTest.java
@@ -3,10 +3,13 @@ package github;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.ex.DialogTextMismatch;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.screenshot;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FindSelenideTest {
 
@@ -14,22 +17,25 @@ public class FindSelenideTest {
     @Test
     void shouldFindSelenideOnGitHub() {
 
+
+
+
         //открыть страницу gitHub
-        Selenide.open("https://github.com");
+        open("https://github.com");
 
         //ввести в поле поиска selenide и нажать enter
-        Selenide.$("[name=q]").setValue("Selenide").pressEnter();
+        $("[name=q]").setValue("Selenide").pressEnter();
 
         //кликнуть на раздел wiki проекта
-        Selenide.$$(".menu-item").get(8).click();
+        $$(".menu-item").get(8).click();
 
         //проверить, что на странице есть SoftAssertions и перейти на неё
-        Selenide.$(Selectors.byText("SoftAssertions")).shouldBe(Condition.visible);
-        Selenide.$("[title='SoftAssertions']").click();
+        $(byText("SoftAssertions")).shouldBe(Condition.visible);
+        $("[title='SoftAssertions']").click();
 
         //проверить текст
-        Selenide.$(Selectors.byText("Using JUnit5 extend test class:")).shouldBe(Condition.visible);
+        $(byText("Using JUnit5 extend test class:")).shouldBe(Condition.visible);
 
 
     }
-}
+    }
